d6830f25e4ebac4643b9c9f0e852f06caa3bd771
Henry Macanas <henry@Henrys-MacBook-Pro.local>
2019-10-28 20:02:23 -0600

    added config file for IntegratedSim and pendulum3D.m

--- pendulum3D.m
+++ pendulum3D.m
@@ -1,31 +1,8 @@
 clc, close, clear
 addpath('helperLibrary');
+addpath('simulink/Simulation');
 
-% vehicle properties SI units
-vp = struct();
-vp.edge = .1;
-vp.rho = 600;
-vp.mass = vp.edge^3*vp.rho;
-vp.J = (1/6*vp.mass*vp.edge^2) * eye(3,3);
-vp.r32 = 1/2 * vp.edge * [1; 1; 1];
-vp.g = 9.81;
-vp.gVect = [0;0;1];
-
-% parallel axis theorem to get inertial tensor in F2
-vp.J = vp.J  - vp.mass*cross_matrix(vp.r32)*cross_matrix(vp.r32);
-
-% q21
-a = cross(vp.r32, vp.gVect);
-a = a/norm(a);
-F = acos(dot(vp.r32, vp.gVect)/(norm(vp.r32)*norm(vp.gVect)));
-F = deg2rad(50);
-eta = cos(F/2);
-eps = a*sin(F/2);
-q21 = [eps;eta];
-
-% initial state
-wBody = deg2rad(0)*[1;1;1]/norm([1;1;1]);
-state0 = [q21; wBody];
+CubliConfig;
 
 % propagate
 options = odeset('RelTol', 1e-8, 'AbsTol',1e-8);
@@ -42,9 +19,7 @@ plot(t, qBody)
 figure
 plot(t, wBody)
 
-simMotion(t, qBody, vp.edge);
-% [phi, theta, psi] = quat2euler(qBody);
-% plot_cube_motion(t(2)-t(1), phi, theta, psi, vp.edge);
+%simMotion(t, qBody, vp.edge);
 
 function [dy] = rigidBodyMotion(t, state, vp)
 
---
+++ simulink/Simulation/CubliConfig.m
@@ -0,0 +1,31 @@
+% vehicle properties SI units
+vp = struct();
+vp.edge = .1;
+vp.rho = 600;
+vp.mass = vp.edge^3*vp.rho;
+vp.J = (1/6*vp.mass*vp.edge^2) * eye(3,3);
+vp.r32 = 1/2 * vp.edge * [1; 1; 1];
+vp.g = 9.81;
+vp.gVect = [0;0;1];
+
+% parallel axis theorem to get inertial tensor in F2
+vp.J = vp.J  - vp.mass*cross_matrix(vp.r32)*cross_matrix(vp.r32);
+
+% q21
+a = cross(vp.r32, vp.gVect);
+a = a/norm(a);
+F = acos(dot(vp.r32, vp.gVect)/(norm(vp.r32)*norm(vp.gVect)));
+F = deg2rad(50);
+eta = cos(F/2);
+eps = a*sin(F/2);
+qBody0 = [eps;eta];
+
+% initial state
+wBody0 = deg2rad(0)*[1;1;1]/norm([1;1;1]);
+Hsys0 = vp.J*wBody0;
+state0 = [qBody0; wBody0];
+
+% sample time
+Ts = .01;
+
+sim('IntegratedSim.slx');
